8aa33bb25ac32ca641bdf4e0d0f6348e87a9c59b
alenprastyaa <1001cerita1001@gmail.com>
2026-04-30 16:14:22 +0700

    ci cd

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.VUE_APP_PUBLIC_PATH || "/",
   devServer: {
     host: "0.0.0.0",
     allowedHosts: "all",
